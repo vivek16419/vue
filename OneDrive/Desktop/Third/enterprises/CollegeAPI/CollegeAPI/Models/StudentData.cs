24418f7dcc4925ee023b402a24f22445e820329f
vivek singh <200361499@student.georgianc.on.ca>
2018-04-09 13:42:32 -0400

    1

---
+++ OneDrive/Desktop/Third/enterprises/CollegeAPI/CollegeAPI/Models/StudentData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeAPI.Models
+{
+    public class StudentData : DbContext
+    {
+        // constructor
+        public StudentData(DbContextOptions<StudentData>options) :base(options)
+        {
+
+        }
+    }
+}
---
+++ OneDrive/Desktop/Third/enterprises/CollegeAPI/CollegeAPI/Models/student_table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeAPI.Models
+{
+    public class student_table
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string BirthDate { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
